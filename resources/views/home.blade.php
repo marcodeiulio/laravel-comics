86e286951f106b94018c71fa5cf485714b728f25
marcodeiulio <mdeiulio@icloud.com>
2022-03-15 19:33:54 +0100

    - Installed npm - Added img - Added header - Added sass stylesheets

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="{{asset('css/app.css')}}">
+	<title>{{env('APP_NAME')}}</title>
+</head>
+
+<body>
+	<header class="container">
+		<figure>
+			<a href="{{route('home')}}"><img src="{{ asset('img/dc-logo.png') }}" alt="DC Logo" /></a>
+		</figure>
+		<nav>
+			<ul>
+				@forelse ($header_menu as $menu)
+				<li>
+					<a href="#">{{$menu['title']}}</a>
+				</li>
+				@empty
+				<h3>Oops... No comics here!</h3>
+				@endforelse
+			</ul>
+		</nav>
+	</header>
+</body>
+
+</html>
