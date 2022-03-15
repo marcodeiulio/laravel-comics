7c9fc4aa7979ec1c90f05deaad7ef7cbdaa62d46
marcodeiulio <mdeiulio@icloud.com>
2022-03-15 20:33:23 +0100

    - Added footer & related data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,6 +57,42 @@
 			</div>
 		</section>
 	</main>
+	<footer>
+		<section id="upper-footer">
+			<div class="container">
+				<div id="footer-links" class="col-2">
+					@foreach ($footer_navs as $nav)
+					<nav>
+						<h3>{{ $nav['title'] }}</h3>
+						<ul>
+							@foreach ($nav['links'] as $link)
+							<li>
+								<a href=""> {{ $link['text'] }} </a>
+							</li>
+							@endforeach
+						</ul>
+					</nav>
+					@endforeach
+				</div>
+				<div id="footer-logo" class="col-2"></div>
+			</div>
+		</section>
+		<section id="lower-footer">
+			<div class="container">
+				<a href="#" class="border">
+					<h2>Sign-up now!</h2>
+				</a>
+				<nav>
+					<h2>Follow Us</h2>
+					@foreach ($footer_socials as $social)
+					<a href="{{$social['href']}}">
+						<img src="{{$social['icon']}}" alt="{{$social['alt']}}" />
+					</a>
+					@endforeach
+				</nav>
+			</div>
+		</section>
+	</footer>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -271,7 +271,84 @@ Route::get('/', function () {
             'url' => '#'
         ],
     ];
+    $footer_socials = [
+        [
+            'href' => "#",
+            'icon' => "asset('img/footer-facebook.png')",
+            'alt' => "Facebook Logo",
+        ],
+        [
+            'href' => "#",
+            'icon' => "asset('img/footer-twitter.png')",
+            'alt' => "Twitter Logo",
+        ],
+        [
+            'href' => "#",
+            'icon' => "asset('img/footer-youtube.png')",
+            'alt' => "YouTube Logo",
+        ],
+        [
+            'href' => "#",
+            'icon' => "asset('img/footer-pinterest.png')",
+            'alt' => "Pinterest Logo",
+        ],
+        [
+            'href' => "#",
+            'icon' => "asset('img/footer-periscope.png')",
+            'alt' => "Periscope Logo",
+        ],
+    ];
+    $footer_navs = [
+        [
+            'title' => "DC Comics",
+            'links' => [
+                ['text' => "characters", 'url' => "#"],
+                ['text' => "comics", 'url' => "#", 'active' => true],
+                ['text' => "movies", 'url' => "#"],
+                ['text' => "tv", 'url' => "#"],
+                ['text' => "games", 'url' => "#"],
+                ['text' => "collectibles", 'url' => "#"],
+                ['text' => "videos", 'url' => "#"],
+                ['text' => "fans", 'url' => "#"],
+                ['text' => "news", 'url' => "#"],
+                ['text' => "shop", 'url' => "#"],
+            ],
+        ],
+        [
+            'title' => "Shop",
+            'links' => [
+                ['text' => "shop dc", 'url' => "#"],
+                ['text' => "shop dc collectibles", 'url' => "#"],
+            ],
+        ],
+        [
+            'title' => "DC",
+            'links' => [
+                ['text' => "terms of use", 'url' => "#"],
+                ['text' => "privacy policy", 'url' => "#"],
+                ['text' => "ad choices", 'url' => "#"],
+                ['text' => "advertising", 'url' => "#"],
+                ['text' => "jobs", 'url' => "#"],
+                ['text' => "subscriptions", 'url' => "#"],
+                ['text' => "talent workshop", 'url' => "#"],
+                ['text' => "CPSC certificates", 'url' => "#"],
+                ['text' => "ratings", 'url' => "#"],
+                ['text' => "shop help", 'url' => "#"],
+                ['text' => "contact us", 'url' => "#"],
+            ],
+        ],
+        [
+            'title' => "sites",
+            'links' => [
+                ['text' => "DC", 'url' => "#"],
+                ['text' => "MAD magazine", 'url' => "#"],
+                ['text' => "DC kids", 'url' => "#"],
+                ['text' => "DC universe", 'url' => "#"],
+                ['text' => "DC power visa", 'url' => "#"],
+            ],
+        ],
+    ];
 
 
-    return view('home', compact('header_menu', 'comics')); //? oppure ['comics' => $comics]
+    return view('home', compact('header_menu', 'comics', 'footer_socials', 'footer_navs')); //? oppure ['comics' => $comics]
 })->name('home');
